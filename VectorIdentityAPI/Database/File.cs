5b766dd12e24ca5f57802b87286a14f2af6bab53
vgrm <vgrmmail@gmail.com>
2021-02-25 18:36:21 +0200

    Init API

---
+++ VectorIdentityAPI/Database/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VectorIdentityAPI.Database
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Original { get; set; }
+        public double Identity { get; set; }
+        public double Correctness { get; set; }
+        public DateTime Date { get; set; }
+
+        public User Owner { get; set; }
+        public Project Project { get; set; }
+        public ICollection<Match> Matches { get; set; }
+    }
+}
---
+++ VectorIdentityAPI/Database/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VectorIdentityAPI.Database
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public int Pointer { get; set; }
+        public double Value { get; set; }
+
+
+
+    }
+}
